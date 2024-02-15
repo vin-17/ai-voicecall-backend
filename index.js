9131e0802c6c8ccf818829872ad593775b95ecf3
Vineet Singh <vineet.yo3@gmail.com>
2024-02-15 21:10:52 +0530

    for deployment

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get('/data', async (req, res) => {
     res.json(data);
 });
 
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+  });
+
 
 app.listen(5000, () => {
     console.log("\n-------------------");
